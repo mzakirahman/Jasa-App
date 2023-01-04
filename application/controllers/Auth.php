12b6f8bbe68f692d47655d9ef05fde120adbaa52
ZakiRahman <mzakiriau@gmail.com>
2023-01-04 20:04:13 +0700

    lanjut

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -68,10 +68,42 @@ class Auth extends CI_Controller
       redirect('auth');
     }
   }
-
-
-
   public function registration()
+  {
+    if ($this->session->userdata('email')) {
+      redirect('user');
+    }
+    $this->form_validation->set_rules('name', 'Name', 'required|trim');
+    $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', ['is_unique' => 'Email ini sudah terdaftar!']);
+    $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+      'matches' => 'Kata sandi tidak cocok!',
+      'min_length' => 'Kata sandi terlalu pendek!'
+    ]);
+    $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+    if ($this->form_validation->run() == false) {
+      $data['title'] = 'Jasa App | Registration';
+      $this->load->view('guest/guest_header.php', $data);
+      $this->load->view('auth/registration.php');
+      $this->load->view('guest/guest_footer.php');
+    } else {
+      $email = $this->input->post('email', true);
+      $data = [
+        'name' => htmlspecialchars($this->input->post('name', true)),
+        'email' => htmlspecialchars($email),
+        'image' => 'default.jpg',
+        'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+        'role_id' => 2,
+        'is_active' => 1,
+        'date_created' => time()
+      ];
+      $this->db->insert('user', $data);
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat! Akun anda telah dibuat. Silakan Login</div>');
+      redirect('auth');
+    }
+  }
+
+
+  public function registration_menggunakan_email()
   {
     if ($this->session->userdata('email')) {
       redirect('user');
