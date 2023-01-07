3846aba4db0e6941ac27ceefa258b16e456af785
ZakiRahman <mzakiriau@gmail.com>
2023-01-08 02:03:53 +0700

    Add Page

---
+++ application/views/home/help.php
@@ -0,0 +1,146 @@
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="https://4.bp.blogspot.com/-aFNmnv6CXlI/WwcPNu8o2jI/AAAAAAAACp4/j8-YK2ou49YcMvz0GP95JedViOfdrUg1QCLcBGAs/s1600/se4.jpg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <div class="breadcrumb__text">
+              <h2>Jasa Elektronik</h2>
+              <div class="breadcrumb__option">
+                <a href="<?= base_url('home') ?>">Home</a>
+                <span>Help</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="container">
+      <div class="accordion mt-3 mb-3" id="accordionExample">
+        <div class="card">
+          <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+              <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Collapsible Group Item #1
+              </button>
+            </h2>
+          </div>
+
+          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Collapsible Group Item #2
+              </button>
+            </h2>
+          </div>
+          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Collapsible Group Item #3
+              </button>
+            </h2>
+          </div>
+          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="col-lg-12">
+        <div class="product__details__tab">
+          <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab" aria-selected="true">Description</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#tabs-2" role="tab" aria-selected="false">Information</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#tabs-3" role="tab" aria-selected="false">Reviews <span>(1)</span></a>
+            </li>
+          </ul>
+          <div class="tab-content">
+            <div class="tab-pane active" id="tabs-1" role="tabpanel">
+              <div class="product__details__tab__desc">
+                <h6>Products Infomation</h6>
+                <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
+                  Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Vivamus
+                  suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam
+                  vehicula elementum sed sit amet dui. Donec rutrum congue leo eget malesuada.
+                  Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur arcu erat,
+                  accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a
+                  pellentesque nec, egestas non nisi. Vestibulum ac diam sit amet quam vehicula
+                  elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus
+                  et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam
+                  vel, ullamcorper sit amet ligula. Proin eget tortor risus.</p>
+                <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem
+                  ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet
+                  elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum
+                  porta. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus
+                  nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                  Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed
+                  porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum
+                  sed sit amet dui. Proin eget tortor risus.</p>
+              </div>
+            </div>
+            <div class="tab-pane" id="tabs-2" role="tabpanel">
+              <div class="product__details__tab__desc">
+                <h6>Products Infomation</h6>
+                <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
+                  Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus.
+                  Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam
+                  sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo
+                  eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.
+                  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent
+                  sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac
+                  diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante
+                  ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                  Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
+                  Proin eget tortor risus.</p>
+                <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem
+                  ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet
+                  elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum
+                  porta. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus
+                  nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+              </div>
+            </div>
+            <div class="tab-pane" id="tabs-3" role="tabpanel">
+              <div class="product__details__tab__desc">
+                <h6>Products Infomation</h6>
+                <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
+                  Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus.
+                  Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam
+                  sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo
+                  eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.
+                  Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent
+                  sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac
+                  diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante
+                  ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                  Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
+                  Proin eget tortor risus.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
