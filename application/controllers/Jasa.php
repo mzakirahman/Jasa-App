f39fbf8ea2a5e861bd4d173818911f82fbc9979c
ZakiRahman <mzakiriau@gmail.com>
2023-01-18 13:52:59 +0700

    add mitrans ke detail jasa

--- application/controllers/Jasa.php
+++ application/controllers/Jasa.php
@@ -3,16 +3,111 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Jasa extends CI_Controller
 {
-
   // ====== Fungsi block untuk setiap halaman 
   public function __construct()
   {
     parent::__construct();
     is_logged_in();
     $this->load->model('Jasa_model');
+    // mitrans 
+    $params = array('server_key' => 'SB-Mid-server-ZAJ7F4rAEdZWQA4OUMyQaph9', 'production' => false);
+    $this->load->library('midtrans');
+    $this->midtrans->config($params);
+    $this->load->helper('url');
+  }
+  public function token()
+  {
+    // fungi mitrans
+    // Required
+    $transaction_details = array(
+      'order_id' => rand(),
+      'gross_amount' => 94000, // no decimal allowed for creditcard
+    );
 
+    // Optional
+    $item1_details = array(
+      'id' => 'a1',
+      'price' => 18000,
+      'quantity' => 3,
+      'name' => "Apple"
+    );
+
+    // Optional
+    $item2_details = array(
+      'id' => 'a2',
+      'price' => 20000,
+      'quantity' => 2,
+      'name' => "Orange"
+    );
+
+    // Optional
+    $item_details = array($item1_details, $item2_details);
+
+    // Optional
+    $billing_address = array(
+      'first_name'    => "Andri",
+      'last_name'     => "Litani",
+      'address'       => "Mangga 20",
+      'city'          => "Jakarta",
+      'postal_code'   => "16602",
+      'phone'         => "081122334455",
+      'country_code'  => 'IDN'
+    );
+
+    // Optional
+    $shipping_address = array(
+      'first_name'    => "Obet",
+      'last_name'     => "Supriadi",
+      'address'       => "Manggis 90",
+      'city'          => "Jakarta",
+      'postal_code'   => "16601",
+      'phone'         => "08113366345",
+      'country_code'  => 'IDN'
+    );
+
+    // Optional
+    $customer_details = array(
+      'first_name'    => "Andri",
+      'last_name'     => "Litani",
+      'email'         => "andri@litani.com",
+      'phone'         => "081122334455",
+      'billing_address'  => $billing_address,
+      'shipping_address' => $shipping_address
+    );
+
+    // Data yang akan dikirim untuk request redirect_url.
+    $credit_card['secure'] = true;
+    //ser save_card true to enable oneclick or 2click
+    //$credit_card['save_card'] = true;
+
+    $time = time();
+    $custom_expiry = array(
+      'start_time' => date("Y-m-d H:i:s O", $time),
+      'unit' => 'minute',
+      'duration'  => 2
+    );
+
+    $transaction_data = array(
+      'transaction_details' => $transaction_details,
+      'item_details'       => $item_details,
+      'customer_details'   => $customer_details,
+      'credit_card'        => $credit_card,
+      'expiry'             => $custom_expiry
+    );
+
+    error_log(json_encode($transaction_data));
+    $snapToken = $this->midtrans->getSnapToken($transaction_data);
+    error_log($snapToken);
+    echo $snapToken;
   }
 
+  public function finish()
+  {
+    $result = json_decode($this->input->post('result_data'));
+    echo 'RESULT <br><pre>';
+    var_dump($result);
+    echo '</pre>';
+  }
   public function alljasa()
   {
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
@@ -25,18 +120,17 @@ class Jasa extends CI_Controller
     $this->load->view('jasa/alljasa', $data);
     $this->load->view('tamplates/ui_footer.php', $data);
   }
-  
+
   public function pesanjasa($id)
   {
     $data['title'] = 'Jasa | Pesan';
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
     // model 
     $data['jasa'] = $this->Jasa_model->getJasaById($id);
-  
+
     $this->load->view('tamplates/ui_header.php', $data);
     $this->load->view('tamplates/ui_sidebar.php', $data);
     $this->load->view('jasa/pesanjasa', $data);
     $this->load->view('tamplates/ui_footer.php', $data);
   }
-
 }
--- application/views/jasa/pesanjasa.php
+++ application/views/jasa/pesanjasa.php
@@ -1,5 +1,17 @@
 <!-- CONTENT START -->
+
+<!-- SCRIPT MITRANS -->
+<script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-zNogHI4yBCDTjR0x"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<!-- AKHIR SCRIPT MITRANS -->
+
 <div class="col-lg-9 col-md-7">
+  <!-- FORM BAWAAN DARI MITRANS YANG BERTYPE HIDDEN -->
+  <form id="payment-form" method="post" action="<?= site_url() ?>/snap/finish">
+    <input type="hidden" name="result_type" id="result-type" value="">
+    <input type="hidden" name="result_data" id="result-data" value="">
+  </form>
   <p><a href="#" class="text-dark">jasa elektronik > </a><a href="#" class="text-primary">Detail Jasa</a>
   <div class="row row-cols-2">
     <div class="col">
@@ -14,12 +26,10 @@
       <h5 class="mt-3"> <i class="fa fa-thumb-tack" aria-hidden="true"></i>
         <?= $jasa['alamat']; ?></h5>
       <p class="text-secondary"><?= $jasa['deskripsi']; ?></p>
-      <button class="site-btn mt-3">Pesan Jasa</button>
+      <button id="pay-button" class="site-btn mt-3">Pesan Jasa</button>
     </div>
   </div>
   <!-- Product Details Section End -->
-
-
 
 
 </div>
@@ -27,4 +37,53 @@
 </div>
 </div>
 </section>
+<!-- SCRIPT MITRANS -->
+<script type="text/javascript">
+  $('#pay-button').click(function(event) {
+    event.preventDefault();
+    $(this).attr("disabled", "disabled");
+
+    $.ajax({
+      url: '<?= site_url() ?>/snap/token',
+      cache: false,
+
+      success: function(data) {
+        //location = data;
+
+        console.log('token = ' + data);
+
+        var resultType = document.getElementById('result-type');
+        var resultData = document.getElementById('result-data');
+
+        function changeResult(type, data) {
+          $("#result-type").val(type);
+          $("#result-data").val(JSON.stringify(data));
+          //resultType.innerHTML = type;
+          //resultData.innerHTML = JSON.stringify(data);
+        }
+
+        snap.pay(data, {
+
+          onSuccess: function(result) {
+            changeResult('success', result);
+            console.log(result.status_message);
+            console.log(result);
+            $("#payment-form").submit();
+          },
+          onPending: function(result) {
+            changeResult('pending', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          },
+          onError: function(result) {
+            changeResult('error', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          }
+        });
+      }
+    });
+  });
+</script>
+<!-- AKHIR SCRIPT MITRANS -->
 <!-- Product Section End -->
