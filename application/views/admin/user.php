f09f81659c93872d7d093b057aba10057431cea5
ZakiRahman <mzakiriau@gmail.com>
2023-04-10 14:17:40 +0700

    perbaiki views

--- application/views/admin/user.php
+++ application/views/admin/user.php
@@ -1,38 +1,37 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
-<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-<table class="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">No</th>
-            <th scope="col">Nama</th>
-            <th scope="col">Email</th>
-            <th scope="col">Profile</th>
-            <th scope="col">Role</th>
-            <th scope="col">Password</th>
-            <!-- <th scope="col">Aktif | Non</th> -->
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php $i = 1; ?>
-          <?php foreach ($user as $user) : ?>
-            <tr>
-              <th scope="row"><?= $i; ?></th>
-              <td><?= $user['name']; ?></td>
-              <td><?= $user['email']; ?></td>
-              <td><img height="30" src="<?= base_url('assets/img/profile/') . $user['image']; ?>" alt=""></td>
-              <td>
-                <?php
-                if ($user['role_id'] == 1) {
-                  echo 'Admin';
-                } else {
-                  echo 'User';
-                }
-                ?>
-              </td>
-              <!-- <td>
+  <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">No</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Email</th>
+        <th scope="col">Profile</th>
+        <th scope="col">Role</th>
+        <th scope="col">Password</th>
+        <!-- <th scope="col">Aktif | Non</th> -->
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php $i = 1; ?>
+      <?php foreach ($user as $user) : ?>
+        <tr>
+          <th scope="row"><?= $i; ?></th>
+          <td><?= $user['name']; ?></td>
+          <td><?= $user['email']; ?></td>
+          <td><img height="30" src="<?= base_url('assets/img/profile/') . $user['image']; ?>" alt=""></td>
+          <td>
+            <?php
+            if ($user['role_id'] == 1) {
+              echo 'Admin';
+            } else {
+              echo 'User';
+            }
+            ?>
+          </td>
+          <!-- <td>
                 <?php
                 if ($user['is_active'] == 1) {
                   echo 'Aktif';
@@ -41,24 +40,19 @@
                 }
                 ?>
               </td> -->
-              <td>
-                <span class="d-inline-block text-truncate" style="max-width: 150px;">
-                  <?= $user['password']; ?>
-                </span>
-              <td>
-                <a href="" class="badge badge-success">edit</a>
-                <a href="" class="badge badge-danger">delete</a>
-              </td>
-            </tr>
-            <?php $i++; ?>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
-
-
-
-
-
+          <td>
+            <span class="d-inline-block text-truncate" style="max-width: 150px;">
+              <?= $user['password']; ?>
+            </span>
+          <td>
+            <a href="" class="badge badge-success">edit</a>
+            <a href="" class="badge badge-danger">delete</a>
+          </td>
+        </tr>
+        <?php $i++; ?>
+      <?php endforeach; ?>
+    </tbody>
+  </table>s
 </div>
 </div>
 <!-- End of Main Content -->
